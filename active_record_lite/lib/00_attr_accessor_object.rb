e4c12f9285001092c687cf0f8f31c65a39e5e809
Jeffrey Woldan <jwoldan@gmail.com>
2016-11-04 11:54:33 -0400

    Remove unnecessary to_s from my_attr_accessor

--- active_record_lite/lib/00_attr_accessor_object.rb
+++ active_record_lite/lib/00_attr_accessor_object.rb
@@ -5,7 +5,7 @@ class AttrAccessorObject
       define_method(name) do
         instance_variable_get("@#{name}")
       end
-      define_method("#{name.to_s}=") do |val|
+      define_method("#{name}=") do |val|
         instance_variable_set("@#{name}", val)
       end
     end

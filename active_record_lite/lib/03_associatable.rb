db177da9182d1b0c8b273360d9c874319b84ecf2
Jeffrey Woldan <jwoldan@gmail.com>
2016-11-04 13:41:00 -0400

    Implement AssocOptions and subclasses

--- active_record_lite/lib/03_associatable.rb
+++ active_record_lite/lib/03_associatable.rb
@@ -10,23 +10,30 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @primary_key = options[:primary_key] || :id
+    @foreign_key = options[:foreign_key] ||
+      "#{name.to_s.downcase}_id".to_sym
+    @class_name = options[:class_name] || name.to_s.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @primary_key = options[:primary_key] || :id
+    @foreign_key = options[:foreign_key] ||
+      "#{self_class_name.downcase}_id".to_sym
+    @class_name = options[:class_name] ||
+      name.to_s.singularize.camelcase
   end
 end
 

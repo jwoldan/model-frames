529fee42f33701ff85eb6876b9068152263aee90
Jeffrey Woldan <jwoldan@gmail.com>
2016-11-15 12:21:41 -0500

    Complete p01_basic_server.rb

--- RailsLite/bin/p01_basic_server.rb
+++ RailsLite/bin/p01_basic_server.rb
@@ -1,1 +1,15 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  # write the request path into the response
+  res.write(req.path)
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
